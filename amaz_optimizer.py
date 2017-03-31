5f08972cfc1dd5d43a4cea4071573287044c8c15
suguru <u.sgr.22114w@gmail.com>
2017-03-31 11:36:51 +0900

    prepared new optimizer following papper

--- amaz_optimizer.py
+++ amaz_optimizer.py
@@ -33,3 +33,22 @@ class OptimizerSqueeze(Optimizers):
             new_lr = self.lr * 0.1
             self.optimizer.lr = new_lr
             print("optimizer was changed to {0}..".format(new_lr))
+
+class OptimizerSqueezeOnPaper(Optimizers):
+    """
+    refering parameters on https://github.com/DeepScale/SqueezeNet/blob/master/SqueezeNet_v1.0/solver.prototxt
+    """
+    def __init__(self,model=None,lr=0.04,momentum=0.9,epoch=300,schedule=(100,200),weight_decay=2.0e-4):
+        super(OptimizerSqueeze,self).__init__(model,epoch)
+        self.lr = lr
+        self.optimizer = optimizers.MomentumSGD(self.lr,momentum)
+        weight_decay = chainer.optimizer.WeightDecay(weight_decay)
+        self.optimizer.setup(model)
+        self.optimizer.add_hook(weight_decay)
+        self.schedule = schedule
+
+    def update_parameter(self,current_epoch):
+        if current_epoch in self.schedule:
+            new_lr = self.lr * 0.1
+            self.optimizer.lr = new_lr
+            print("optimizer was changed to {0}..".format(new_lr))
